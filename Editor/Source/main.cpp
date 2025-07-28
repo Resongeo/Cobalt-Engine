f1cf692796e600f5f578213601630dc798759d75
Resongeo <s.benedek2003@gmail.com>
2025-07-28 18:42:46 +0200

    chore: setup project structure

---
+++ Editor/Source/main.cpp
@@ -0,0 +1,6 @@
+#include "test.hpp"
+#include <print>
+
+auto main() -> int {
+    std::println("Test from engine: {}", add(10, 2));
+}
---
+++ Engine/Source/test.cpp
@@ -0,0 +1,5 @@
+#include "test.hpp"
+
+auto add(const int a, const int b) -> int {
+    return a + b;
+}
