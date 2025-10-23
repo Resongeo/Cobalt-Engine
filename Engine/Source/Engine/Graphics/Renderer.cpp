a79947d27dc1e44b254a646fd55bce9ae32e3fc2
Resongeo <s.benedek2003@gmail.com>
2025-10-23 21:15:28 +0200

    fix(renderer): reinterpret const char* so it works on Windows

--- Engine/Source/Engine/Graphics/Renderer.cpp
+++ Engine/Source/Engine/Graphics/Renderer.cpp
@@ -16,8 +16,8 @@ namespace Cobalt::Engine
         const auto vertex_path = shaders_path / "DefaultQuad.vert";
         const auto fragment_path = shaders_path / "DefaultQuad.frag";
         auto result = m_default_shader->create_from_file(
-            vertex_path.c_str(),
-            fragment_path.c_str()
+            reinterpret_cast<const char*>(vertex_path.c_str()),
+            reinterpret_cast<const char*>(fragment_path.c_str())
         );
         if (!result) {
             Logger::error("Engine::Shader", "Failed to create default quad shader.");
