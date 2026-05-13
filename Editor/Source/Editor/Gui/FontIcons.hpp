6a03749663daa7457de32dfd7f2c9960fbc7c5a1
Resongeo <s.benedek2003@gmail.com>
2026-05-13 16:51:15 +0200

    feat: add font icon codepoints

---
+++ Editor/Source/Editor/Gui/FontIcons.hpp
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2026 Somogyvári Benedek
+
+#pragma once
+
+namespace Cobalt
+{
+#define ICON_RANGE_MIN 0xe038
+#define ICON_RANGE_MAX 0xe695
+
+#define ICON_COMPONENT   "\xee\x8a\xad" // U+e2ad
+#define ICON_LAYOUT_LIST "\xee\x87\x99"	// U+e1d9
+#define ICON_REFRESH     "\xee\x85\x88"	// U+e148
+} // namespace Cobalt
