d332828b6c38c2789e607dc3d2196de4751b439a
Resongeo <s.benedek2003@gmail.com>
2026-06-30 13:16:43 +0200

    feat(editor): show thumbnail and script name in EntityComponentsPanel

--- Editor/Source/Editor/Gui/Panels/EntityComponentsPanel.cpp
+++ Editor/Source/Editor/Gui/Panels/EntityComponentsPanel.cpp
@@ -7,6 +7,8 @@
 #include "Editor/Gui/FontIcons.hpp"
 #include "Engine/ECS/Components/Minimal.hpp"
 #include "Engine/ECS/Entity.hpp"
+#include "Engine/Assets/AssetManager.hpp"
+#include "Engine/Graphics/Texture2D.hpp"
 
 #include <imgui.h>
 #include <misc/cpp/imgui_stdlib.h>
@@ -16,6 +18,8 @@ namespace Cobalt
     auto EntityComponentsPanel::Draw(EditorState& state) -> void {
         Widgets::Begin("Components", {8, 8});
         {
+            auto& imgui_style = ImGui::GetStyle();
+
             if (state.selected_entity != entt::null) {
                 auto entity = Entity(state.selected_entity, &state.active_scene->GetRegistry());
 
@@ -72,7 +76,16 @@ namespace Cobalt
                     if (Widgets::CollapsingHeader("Sprite", Colors::sprite)) {
                         auto& [tint, uuid] = entity.GetComponent<SpriteComponent>();
                         ImGui::ColorEdit4("Tint", &tint.r);
-                        ImGui::InputScalar("Texture UUID", ImGuiDataType_U64, (void*)&uuid.value);
+
+                        const auto uuid_str = std::to_string(uuid.value);
+                        const auto region_avail = ImGui::GetContentRegionAvail();
+                        const auto cursor_pos = ImGui::GetCursorScreenPos();
+                        const auto draw_list = ImGui::GetWindowDrawList();
+                        constexpr auto thumbnail_size = ImVec2{60, 60};
+                        constexpr auto thumbnail_padding = 10.0f;
+                        constexpr auto rect_height = thumbnail_size.y + thumbnail_padding * 2.0f;
+
+                        ImGui::InvisibleButton(uuid_str.c_str(), {region_avail.x, rect_height});
 
                         if (ImGui::BeginDragDropTarget()) {
                             if (const auto* payload = ImGui::AcceptDragDropPayload("ASSET_DRAG_AND_DROP")) {
@@ -81,13 +94,55 @@ namespace Cobalt
                             }
                             ImGui::EndDragDropTarget();
                         }
+
+                        draw_list->AddRectFilled(
+                            cursor_pos,
+                            cursor_pos + ImVec2{region_avail.x, rect_height},
+                            ImGui::GetColorU32(imgui_style.Colors[ImGuiCol_FrameBg]),
+                            imgui_style.FrameRounding
+                        );
+
+                        const auto texture = AssetManager::Get().GetAsset<Texture2D>(uuid);
+                        const auto text_line_height = ImGui::GetTextLineHeight();
+                        const auto text_y_offset = rect_height * 0.5f - text_line_height * 0.5f;
+                        if (texture) {
+                            ImGui::SetCursorScreenPos(cursor_pos + ImVec2{thumbnail_padding, thumbnail_padding});
+                            ImGui::Image(texture->GetRendererID(), thumbnail_size);
+
+                            ImGui::SetCursorScreenPos(cursor_pos + ImVec2{thumbnail_size.x + thumbnail_padding * 2, text_y_offset});
+                            const auto meta = AssetManager::Get().GetRegistry().GetMetadata(uuid).value();
+                            const auto texture_name_string = meta.path.filename().string();
+                            ImGui::Text("%s", texture_name_string.c_str());
+                        } else {
+                            const auto min = cursor_pos + ImVec2{thumbnail_padding, thumbnail_padding};
+                            draw_list->AddRectFilled(
+                                min,
+                                min + thumbnail_size,
+                                IM_COL32_WHITE
+                                );
+
+                            ImGui::SetCursorScreenPos(cursor_pos + ImVec2{thumbnail_size.x + thumbnail_padding * 2, text_y_offset});
+                            ImGui::Text("Default Texture");
+                        }
+
+                        ImGui::SetCursorScreenPos(cursor_pos);
+                        ImGui::Dummy({0, rect_height});
                     }
                 }
 
                 if (entity.HasComponent<ScriptComponent>()) {
                     if (Widgets::CollapsingHeader("Script", Colors::script)) {
                         auto& [uuid, _] = entity.GetComponent<ScriptComponent>();
-                        ImGui::InputScalar("Script UUID", ImGuiDataType_U64, (void*)&uuid.value);
+
+                        const auto uuid_str = std::to_string(uuid.value);
+                        const auto region_avail = ImGui::GetContentRegionAvail();
+                        const auto cursor_pos = ImGui::GetCursorScreenPos();
+                        const auto draw_list = ImGui::GetWindowDrawList();
+                        constexpr auto thumbnail_padding = 10.0f;
+                        const auto text_line_height = ImGui::GetTextLineHeight();
+                        const auto rect_height = text_line_height + thumbnail_padding * 2.0f;
+
+                        ImGui::InvisibleButton(uuid_str.c_str(), {region_avail.x, rect_height});
 
                         if (ImGui::BeginDragDropTarget()) {
                             if (const auto* payload = ImGui::AcceptDragDropPayload("ASSET_DRAG_AND_DROP")) {
@@ -96,6 +151,27 @@ namespace Cobalt
                             }
                             ImGui::EndDragDropTarget();
                         }
+
+                        draw_list->AddRectFilled(
+                            cursor_pos,
+                            cursor_pos + ImVec2{region_avail.x, rect_height},
+                            ImGui::GetColorU32(imgui_style.Colors[ImGuiCol_FrameBg]),
+                            imgui_style.FrameRounding
+                        );
+
+                        auto class_name = String{};
+                        if (auto meta_opt = AssetManager::Get().GetRegistry().GetMetadata(uuid)) {
+                            auto& meta = meta_opt.value();
+                            class_name = meta.path.filename().string();
+                        } else {
+                            class_name = "None";
+                        }
+
+                        ImGui::SetCursorScreenPos(cursor_pos + ImVec2(thumbnail_padding, thumbnail_padding));
+                        ImGui::Text("Script Class: %s", class_name.c_str());
+
+                        ImGui::SetCursorScreenPos(cursor_pos);
+                        ImGui::Dummy({0, rect_height});
                     }
                 }
             }
